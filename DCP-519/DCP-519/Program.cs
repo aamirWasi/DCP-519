c71b31cb04d7d52506ad51891150e5d379052245
aamir <wasi3aamir@gmail.com>
2019-01-27 01:12:58 +0600

    Not Accepted

---
+++ DCP-519/DCP-519/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCP_519
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int t = 1; t <= T; t++)
+            {
+                long x = 2;
+                int n, i;
+                n = int.Parse(Console.ReadLine());
+                while (n > 0)
+                {
+                    for (i = 2; i < x; i++)
+                    {
+                        if (x % i == 0) break;
+                    }
+                    if (x == i)
+                    {
+                        Console.Write($"Caso {t}-> {x.ToString()} ");
+                        n--;
+                    }
+                    x++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
